0b8b12c95f69e2b5611097638196d669d97d6e17
aaditya chapagain <chapagain.aaditya@gmail.com>
2021-06-28 18:28:28 +0545

    adding setting for credentials file

--- cmd/config.go
+++ cmd/config.go
@@ -5,6 +5,9 @@ import "os"
 const switchConfigPath = ".config/gitentry"
 const ghConfigdir = ".config/gh"
 const ghConfigFile = "hosts.yml"
+
+// const regarding user credentials
 const credentialFormatter = "https://%s:%s@github.com"
+const credentialCacheFile = ".configcredentials/cache.out"
 
 var home, _ = os.UserHomeDir()
